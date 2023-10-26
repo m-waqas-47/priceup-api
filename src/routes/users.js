1586f58367a1f78325b29fc1d05eee9761ab66b4
pak993322 <pak993322@gmail.com>
2023-10-26 06:16:36 -0700

    changes

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,12 +11,13 @@ const {
 } = require("../controllers/user");
 const { verifyToken } = require("../middlewares/authentication");
 const router = express.Router();
+const {updateUsers,uploadUsers} = require("../utils/userMulter");
 
 router.get("/getQuote/:id", verifyToken, getQuote);
 router.get("/", verifyToken, getAll);
 router.get("/dashboardData", verifyToken, getDashboardTotals);
 router.get("/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUser);
+router.put("/:id",updateUsers.single("image"), verifyToken, updateUser);
 router.post("/save", verifyToken, saveUser);
 router.put("/status/:id", verifyToken, updateUserStatus);
 router.post("/login", loginUser);
