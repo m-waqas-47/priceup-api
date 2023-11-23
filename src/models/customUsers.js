b91cccbb6f952d0db30735653eadc5798997c640
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2023-11-23 18:37:38 +0500

    remove password from customUser model

--- src/models/customUsers.js
+++ src/models/customUsers.js
@@ -15,11 +15,6 @@ const customUserSchema = new mongoose.Schema(
       match: /.+\@.+\..+/,
       unique: true,
     },
-    password: {
-      type: String,
-      required: "Password is required",
-      minlength: [6, "Password must be atleast 6 character long"],
-    },
     image: {
       type: String,
       default: "images/users/default.jpg",
@@ -48,11 +43,11 @@ const customUserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-customUserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+// customUserSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// });
 
 customUserSchema.methods.comparePassword = function (password) {
   return this.locationsAccess.find((item) =>
