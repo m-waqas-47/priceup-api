723ff855c7fe8dbeea8f290b58335318f1de7df6
Eng-Abdul-Rehman <eng.mirza.rehman@gmail.com>
2024-03-26 10:28:01 +0500

    invalid user id

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -3,8 +3,8 @@ const {
   getAll,
   // loginAdmin,
   saveAdmin,
-  loginAdminById,
-  loginAdminByIdAgain,
+  switchLocation,
+  switchBackToSuperView,
   allLocations,
   updateAdmin,
   deleteAdmin,
@@ -19,8 +19,8 @@ router.get("/allLocations", verifyToken, allLocations);
 router.put("/updatePassword/:id", verifyToken, updateAdminPassword);
 router.put("/:id", verifyToken, upload.single("image"), updateAdmin);
 router.delete("/:id", verifyToken, deleteAdmin);
-router.post("/loginAdminId", verifyToken, loginAdminById);
-router.post("/loginAdminIdAgain", verifyToken, loginAdminByIdAgain);
+router.post("/switchLocation", verifyToken, switchLocation);
+router.post("/switchBackToSuperView", verifyToken, switchBackToSuperView);
 // router.post("/login", loginAdmin);
 router.post("/save",verifyToken, upload.single("image"), saveAdmin);
 module.exports = router;
