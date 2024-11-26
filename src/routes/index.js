7fc41acbd34dab574facfed03c077ec0488c6889
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-11-26 18:59:40 +0500

    testing webhook call

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,5 +11,18 @@ router.get("/", (req, res) => {
 router.get("/dashboard-stats", verifyToken, getDashboardStats);
 router.get("/dashboard-graph-data", verifyToken, getDashboardGraphData);
 router.post("/login", loginUser);
+router.post("/form-submitted-webhook",(req,res) => {
+  const data = {...req.body};
+  try{
+    console.log(data,'data');
+    return res.status(200).json(data);
+  }
+  catch(err){
+   return res.status(400).json({
+      code: statusCode,
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
