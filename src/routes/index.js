6d0e4c79e760daf790c86b29d1145db9fa2a05e3
Muhammad Waqas <muhammadwaqas3447@gmail.com>
2024-11-27 11:19:59 +0500

    remove commented code

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,7 @@ const rateLimitMiddleware = require("@middlewares/requestRateLimit");
 const { getCustomerRequest, getLocations, getLocationData, updateCustomerRequest, formSubmittedWebhook } = require("@controllers/formRequest");
 const { requiredProps } = require("@config/common");
 const { validateRequiredProps } = require("@middlewares/validator");
-// const { handleError, handleResponse } = require("@utils/responses");
 const router = express.Router();
-// const axios = require("axios"); // Ensure axios is imported if not already
 const { getCustomerInvoicePreview } = require("@controllers/customerInvoicePreview");
 const { runCustomScripts } = require("@controllers/customScripts");
 
@@ -26,37 +24,5 @@ router.post("/form-submitted-webhook", formSubmittedWebhook);
 router.post("/form-request", rateLimitMiddleware(5 * 60 * 1000, 50), validateRequiredProps(requiredProps.FORMREQUEST), getCustomerRequest);
 router.post("/form-request-update",updateCustomerRequest);
 router.post("/login", loginUser);
-// router.post("/create-test-customer", async (req,res) => {
-//   const url = `https://services.leadconnectorhq.com/opportunities/pipelines?locationId=wMf03l211vQKvcwKgDYJ`;
-//   const headers = {
-//     Authorization: `Bearer pit-e3e78855-c6a4-4218-9688-df804af80e8c`,
-//     Version: "2021-07-28",
-//     "Content-Type": "application/json",
-//   };
-//   const data = {
-//     firstName: "34231",
-//     lastName: "45343rwrw2",
-//     name: "3fs23r32fwe2",
-//     email: "someone@something1.com",
-//     locationId: "wMf03l211vQKvcwKgDYJ",
-//     gender: "male",
-//     phone: "2332423432423433",
-//     address1: "324324324343432dfdsfsdf",
-//     city: "Dolomite",
-//     state: "AL",
-//     postalCode: "35061",
-//     timezone: "America/Chihuahua",
-//     source: "public api",
-//     tags: ["contact created from api"],
-//     country: "US",
-//   };
-//   try{
-//    const resp =  await axios.get(url, { headers });
-//    handleResponse(res,200,"sdss",resp.data.pipelines);
-//   }
-//   catch(err){
-//     handleError(res,err);
-//   }
-// })
 
 module.exports = router;
